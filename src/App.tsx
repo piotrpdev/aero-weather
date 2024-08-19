30a1ffc5fd8687bf48d4302229fb5b7c02fc11b1
Piotr Płaczek <piotrpdev@gmail.com>
2024-08-19 15:29:34 +0100

    feat: template runs
    Added biome and pre-commit hooks

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+	return (
+		<div className="container">
+			<h1>Welcome to Tauri!</h1>
+
+			<div className="row">
+				<a href="https://tauri.app" target="_blank" rel="noreferrer">
+					<img
+						src="/tauri.svg"
+						className="logo tauri"
+						alt="Tauri logo"
+					/>
+				</a>
+			</div>
+			<p>Click on the Tauri logo to learn more.</p>
+		</div>
+	);
+}
+
+export default App;
