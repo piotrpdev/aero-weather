8ab0de098edcda4834e88ebfb5ae7a7bc5bd296b
Piotr Płaczek <piotrpdev@gmail.com>
2024-08-20 00:06:49 +0100

    feat: transparent window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@
 
 use tauri::Manager;
 use tauri_plugin_decorum::WebviewWindowExt;
+use window_vibrancy::apply_acrylic;
 
 fn main() {
     tauri::Builder::default()
@@ -13,6 +14,14 @@ fn main() {
             // On macOS it expects a hiddenTitle: true and titleBarStyle: overlay
             let main_window = app.get_webview_window("main").unwrap();
             main_window.create_overlay_titlebar().unwrap();
+
+            #[cfg(target_os = "windows")]
+            apply_acrylic(&main_window, Some((18, 18, 18, 125)))
+                .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+
+            #[cfg(target_os = "macos")]
+            apply_vibrancy(&main_window, NSVisualEffectMaterial::HudWindow, None, None)
+                .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
 
             #[cfg(target_os = "macos")]
             main_window.set_traffic_lights_inset(16.0, 20.0).unwrap();
