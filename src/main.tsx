c4273373408baa9b0e77a26e6423e5c1e9fdb9e4
Piotr Płaczek <piotrpdev@gmail.com>
2024-08-27 22:46:54 +0100

    feat: normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// TODO: Import reset css
 import "@fontsource-variable/roboto-flex/full.css";
+import "normalize.css/normalize.css";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
